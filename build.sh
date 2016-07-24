b8f853ebf7fd072d1fb5bf09d36fbc3358790b24
Stefanescu Cristian <howerkraft@gmail.com>
2016-07-24 18:50:12 +0300

    add build and makedefconf
    Signed-off-by: Stefanescu Cristian <howerkraft@gmail.com>

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+export PATH=/home/cristi/kb/arm-eabi-4.8/bin:$PATH
+export ARCH=arm
+export SUBARCH=arm
+export CROSS_COMPILE=/home/cristi/kb/arm-eabi-4.8/bin/arm-eabi-
+make hammerhead_defconfig
+make -j8
