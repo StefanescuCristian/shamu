bae09a5741403ac9f94f9eb184dc6c243e58854b
Stefanescu Cristian <howerkraft@gmail.com>
2016-07-24 19:52:50 +0300

    update
    Signed-off-by: Stefanescu Cristian <howerkraft@gmail.com>

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@ export PATH=/home/cristi/kb/arm-eabi-4.8/bin:$PATH
 export ARCH=arm
 export SUBARCH=arm
 export CROSS_COMPILE=/home/cristi/kb/arm-eabi-4.8/bin/arm-eabi-
+make mrproper
 make shamu_defconfig
-make -j8
+make -j12
