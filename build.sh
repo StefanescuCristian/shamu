56f6158a71d95a580b62acb03a99cbd15a4940ac
Stefanescu Cristian <howerkraft@gmail.com>
2016-07-25 23:22:05 +0300

    use linaro gcc 4.9.4
    http://forum.xda-developers.com/showthread.php?t=2098133
    
    Signed-off-by: Stefanescu Cristian <howerkraft@gmail.com>

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-export PATH=/home/cristi/kb/arm-eabi-4.8/bin:$PATH
+export PATH=/home/cristi/kb/arm-eabi-4.9.4/bin:$PATH
 export ARCH=arm
 export SUBARCH=arm
-export CROSS_COMPILE=/home/cristi/kb/arm-eabi-4.8/bin/arm-eabi-
+export CROSS_COMPILE=/home/cristi/kb/arm-eabi-4.9.4/bin/arm-eabi-
 rm -f arch/arm/boot/zImage*
 make mrproper
 make shamu_defconfig
