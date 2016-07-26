3c38561ea8441887afc0ff79b1db016b96a4cabc
Stefanescu Cristian <howerkraft@gmail.com>
2016-07-26 18:32:22 +0300

    compile with ccache
    Signed-off-by: Stefanescu Cristian <howerkraft@gmail.com>

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 export PATH=/home/cristi/kb/arm-eabi-4.9.4/bin:$PATH
 export ARCH=arm
 export SUBARCH=arm
-export CROSS_COMPILE=/home/cristi/kb/arm-eabi-4.9.4/bin/arm-eabi-
+export CROSS_COMPILE="ccache /home/cristi/kb/arm-eabi-4.9.4/bin/arm-eabi-"
 rm -f arch/arm/boot/zImage*
 make mrproper
 make shamu_defconfig
