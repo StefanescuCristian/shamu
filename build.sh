800a5d968ba599d147f8f80d1bd03fdf254b6651
Stefanescu Cristian <howerkraft@gmail.com>
2016-07-27 22:28:51 +0300

    update build script
    Signed-off-by: Stefanescu Cristian <howerkraft@gmail.com>

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ export CROSS_COMPILE="ccache /home/cristi/kb/arm-eabi-4.9.4/bin/arm-eabi-"
 
 #cleanup to be sure that we don't build the same kernel
 echo -e "\e[92mCleaning the build directory\e[0m"
-rm -f arch/arm/boot/zImage* ../boot-n.img ../zImage*
+rm -f arch/arm/boot/zImage* boot-n.img zImage*
 make mrproper > /dev/null 2>&1
 
 #build
@@ -20,10 +20,10 @@ make -j12
 if  [[ -a arch/arm/boot/zImage-dtb ]]; then
     echo -e "\e[92mMaking the flashable boot image\e[0m"
     echo -e "\e[92mCopying the kernel image\e[0m"
-    cp arch/arm/boot/zImage-dtb ../
+    cp arch/arm/boot/zImage-dtb .
     echo -e "\e[92mProcessing the image and ramdisk\e[0m"
-    ../mkbootimg --kernel ../zImage-dtb --ramdisk ../ramdisk.gz --cmdline 'console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=shamu msm_rtb.filter=0x37 ehci-hcd.park=3 utags.blkdev=/dev/block/platform/msm_sdcc.1/by-name/utags utags.backup=/dev/block/platform/msm_sdcc.1/by-name/utagsBackup coherent_pool=8M' --base  0x00000000 --pagesize 2048 --ramdisk_offset 0x0000000b --tags_offset 0x0000000b -o ../boot-n.img
-    if [[ -a ../boot-n.img ]]; then
+    ./mkbootimg --kernel zImage-dtb --ramdisk ramdisk.gz --cmdline 'console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=shamu msm_rtb.filter=0x37 ehci-hcd.park=3 utags.blkdev=/dev/block/platform/msm_sdcc.1/by-name/utags utags.backup=/dev/block/platform/msm_sdcc.1/by-name/utagsBackup coherent_pool=8M' --base  0x00000000 --pagesize 2048 --ramdisk_offset 0x0000000b --tags_offset 0x0000000b -o boot-n.img
+    if [[ -a boot-n.img ]]; then
         echo -e "\e[92mDone building, happy flashing!\e[0m"
     else
         echo -e "\e[31mBuild stuck in processing the ramdisk and kernel!\e[0m"
